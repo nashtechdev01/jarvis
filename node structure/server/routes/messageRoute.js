66029b52b930ef400b7d40c7747502feee81c4e4
NashTech Dev01 <nashtech.dev01@harveynash.vn>
2016-08-16 17:30:32 +0700

    update layout chatbox

--- node structure/server/routes/messageRoute.js
+++ node structure/server/routes/messageRoute.js
@@ -5,28 +5,29 @@ var messageService = require('../services/messageService');
 var router = express.Router();
 var config = require('config');
 
-router.get('/', function (req, res, next) {
+router.get('/', function(req, res, next) {
     messageService.getMessages()
-        .then(function (result) {
+        .then(function(result) {
             res.send(result);
         })
-        .catch(function (err) {
+        .catch(function(err) {
             res.send(err);
         });
 });
 
-router.get('/add', function (req, res, next) {
+router.get('/add', function(req, res, next) {
     var obj = {
         name: req.query.name,
         content: req.query.content
     }
     messageService.addMessage(obj)
-        .then(function (result) {
+    .then(function(result) {
             res.send(result);
         })
-        .catch(function (err) {
+        .catch(function(err) {
             res.send(err);
         });
+
 });
 
 module.exports = router;
